cfc97b5eaa6515aa630e5a22230aad3bc8fc2338
NikolasFRI <nikolas.friedlmayer@gmail.com>
2025-09-02 21:27:21 +0200

    changed workflow

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,16 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
 import mantine from 'eslint-config-mantine';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(...mantine, { ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}'] }, {
-  files: ['**/*.story.tsx'],
-  rules: { 'no-console': 'off' },
-}, {
-  rules: {
-    curly: 'off',
+export default tseslint.config(
+  ...mantine,
+  { ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}'] },
+  {
+    files: ['**/*.story.tsx'],
+    rules: { 'no-console': 'off' },
   },
-}, storybook.configs["flat/recommended"]);
+  {
+    rules: {
+      curly: 'off',
+    },
+  }
+);
